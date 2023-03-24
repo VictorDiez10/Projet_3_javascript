d2410ee3820f28c247dc8de773ded06749e04581
Victor Diez <victordiez1@gmail.com>
2023-03-24 17:03:32 +0100

    Etape 1.1 : récupération des travaux depuis le Back-end grace a fetch

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,13 @@
-const img = document.createElement('img')
 
-fetch('http://localhost:5678/api/works')
-.then(res => res.json())
-.then(data => console.log(data))
+
+fetch('http://localhost:5678/api/works') /* appel à l'api*/
+.then(reponse => reponse.json())
+.then(data => {
+    /* affichage des images grace à l'api*/
+    console.log(data);
+    let figure = '';
+    for (let images of data){
+        figure += `<figure><img src="${images.imageUrl}"></figure>`
+    }
+    document.querySelector('.gallery').innerHTML = figure;
+})
