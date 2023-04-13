81812bc20e1f3173571d2f246f9f556cedf8051f
Victor Diez <victordiez1@gmail.com>
2023-04-13 17:52:22 +0200

    bouton fonctionnel avec propriétés css, bouton "Tout" à configurer pour qu'il fasse apparître toute la galerie

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,60 +1,69 @@
 // Fonction Fetch pour récupérer les données de l'API
-let data = []
+let data = [];
 async function fetchCard() {
-    const response = await fetch("http://localhost:5678/api/works");
-    data = await response.json(); 
-    createCard(data);
-} 
+  const response = await fetch("http://localhost:5678/api/works");
+  data = await response.json();
+  createCard(data);
+}
 fetchCard();
 
-let categoryData = []
+let categoryData = [];
 async function fetchCategory() {
-    const response = await fetch("http://localhost:5678/api/categories");
-    categoryData = await response.json();
-    createButton(categoryData);
+  const response = await fetch("http://localhost:5678/api/categories");
+  categoryData = await response.json();
+  createButton(categoryData);
 }
 fetchCategory();
 
 // Fonction pour afficher les différentes travaux avec leurs noms
-function createCard (works) {
-    for (let i = 0; i < works.length; i++){
-        const figure = document.createElement("figure");
-        const image = document.createElement("img");
-        image.src = works[i].imageUrl;
-        image.setAttribute("alt", works[i].title);
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerText = works[i].title;
-        
-        //DOM pour lier le HTML
-        document.querySelector(".gallery").appendChild(figure);
-        figure.appendChild(image);
-        figure.appendChild(figcaption);
-    }
-};
+function createCard(works) {
+  for (let i = 0; i < works.length; i++) {
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    image.src = works[i].imageUrl;
+    image.setAttribute("alt", works[i].title);
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerText = works[i].title;
+
+    //DOM pour lier le HTML
+    document.querySelector(".gallery").appendChild(figure);
+    figure.appendChild(image);
+    figure.appendChild(figcaption);
+  }
+}
 
 // Création des 4 boutons  avec leurs ID
 
-const buttonTout = document.createElement("button");
-buttonTout.setAttribute("id", "Tout")
-buttonTout.innerText = "Tout";
+function createButton(category) {
+  const buttonTout = document.createElement("button");
+  buttonTout.setAttribute("id", "Tout");
+  buttonTout.innerText = "Tout";
 
-document.querySelector(".filtre").appendChild(buttonTout);
+  document.querySelector(".filtre").appendChild(buttonTout);
+
+  for (let i = 0; i < category.length; i++) {
+    const button = document.createElement("button");
+    button.setAttribute("id", category[i].name);
+    button.innerText = category[i].name;
+
+    document.querySelector(".filtre").appendChild(button);
+    // Ajout du gestionnaire d'événements "click" au bouton
+    button.addEventListener("click", function () {
+      const objetsWorks = data.filter(
+        (work) => work.category.name === category[i].name
+      );
+      document.querySelector(".gallery").innerHTML = "";
+      createCard(objetsWorks);
+    });
+  }
+  const butonActivate = document.querySelectorAll("button");
+
+  butonActivate.forEach(btnActive => {
+    btnActive.addEventListener("click", () => {
+      document.querySelector(".activate")?.classList.remove("activate");
+      btnActive.classList.add("activate");
+    });
+  });
+}
 
 
-function createButton (category) {
-    for (let i = 0; i < category.length; i++){
-        const button = document.createElement("button");
-        button.setAttribute("id", category[i].name);
-        button.innerText = category[i].name;
-
-        document.querySelector(".filtre").appendChild(button);
-    }
-};
-
-
-
-const buttonObjetsTrier = document.querySelector("#Objets");
-buttonObjetsTrier.addEventListener("click", function () {
-    const imagesFiltrees = data.filter (data => data.category.id === category.id);
-    document.querySelector('.gallery').innerHTML = "";
-});
