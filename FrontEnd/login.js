88c89aa6061fc966fbc72aca56f7f039eb331807
Victor Diez <victordiez1@gmail.com>
2023-04-20 18:50:42 +0200

    debut etape 2.2 du login

---
+++ FrontEnd/login.js
@@ -0,0 +1,7 @@
+let response = await fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+  });
