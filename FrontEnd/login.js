e143b034552da7fb95c2e0e85e952258628251c1
VictorDiez10 <victordiez1@gmail.com>
2023-05-02 16:09:35 +0200

    étape 2.2 : authentification de l'utilisateur et peaufinage de 2.1

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,7 +1,23 @@
-let response = await fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-  });
+document.getElementById("se_connecter").addEventListener("click", function() {
+ login();
+})
+
+
+async function login() {
+ const data = {email: document.getElementById("email").value, password: document.getElementById("mdp").value};
+ const response = await fetch("http://localhost:5678/api/users/login", {
+   method: 'POST',
+   headers: {'Content-Type': 'application/json'},
+   body: JSON.stringify(data)
+ });
+ console.log(response);
+ const resultToken = await response.json();
+
+ if (response.status === 200){
+    window.localStorage.setItem("token", resultToken.token);
+    window.location.assign("index.html");
+    console.log("vous êtes connectés");
+ }else{
+   console.log("Désolé, vous avez saisi une adresse e-mail ou un mot de passe incorrect.");
+ }
+}
