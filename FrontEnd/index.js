06acc5179b766e3a3de5008091a1cd70d76fc4b5
Victor Diez <victordiez1@gmail.com>
2023-05-19 00:18:38 +0200

    étape 3.1 : création de la modal

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -106,3 +106,49 @@ if (userToken) {
   const modeEdition = document.querySelector("#mode_edition");
   modeEdition.remove();
 }
+
+
+
+//Fenêtre modal
+//Ouverture de la fenêtre modal
+
+let modal = null
+
+
+const openModal = function(e) {
+  e.preventDefault()
+  const target = document.querySelector(e.target.getAttribute("href"))
+  target.style.display = null
+  target.removeAttribute("aria-hidden")
+  target.setAttribute("aria-modal", "true")
+  modal = target
+  modal.addEventListener("click", closeModal)
+  modal.querySelector(".js-modal-close")?.addEventListener("click", closeModal)
+  modal.querySelector(".js-modal-stop")?.addEventListener("click", stopPropagation)
+}
+
+
+//Fermeture de la fenêtre modal
+
+const closeModal = function(e) {
+  if (modal === null) return
+  e.preventDefault()
+  modal.style.display = "none"
+  modal.setAttribute("aria-hidden", "true")
+  modal.removeAttribute("aria-modal")
+  modal.removeEventListener("click", closeModal)
+  modal.querySelector(".js-modal-close")?.removeEventListener("click", closeModal)
+  modal.querySelector(".js-modal-stop")?.removeEventListener("click", stopPropagation)
+  modal = null
+}
+
+const stopPropagation = function(e) {
+  e.stopPropagation()
+}
+
+
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal)
+})
+
+
